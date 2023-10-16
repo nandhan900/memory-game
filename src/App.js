28148b1121e3b2778d235e69ed85140d970f7d29
nandhan900 <nandhanks00@gmail.com>
2023-10-16 17:50:49 +0530

    memory-game

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Cards from "./components/cards";
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Memory Game - React</h1>
+      <Cards/>
+    </div>
+  );
+}
+
+export default App;
